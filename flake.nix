e4406c5cc560ffbc862f125ce666a5e7d6ba0a64
Michal <michal@tar.black>
2022-07-03 23:17:29 +0000

    Added flake.nix

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+  };
+  
+  outputs = { self, nixpkgs, utils, naersk }:
+    utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages."${system}";
+      naersk-lib = naersk.lib."${system}";
+    in rec
+    {
+      packages.appendage = naersk.lib.buildPackage {
+        pname = "apd";
+        root = "./.";
+      };
+        
+      defaultPackage = packages.appendage;
+        
+      apps.appendage = utils.lib.mkApp {
+        drv = packages.appendage;  
+      };
+        
+      apps.default = apps.appendage;
+        
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          rustc
+          cargo
+          rustfmt
+          clippy
+        ];
+      };
+    });
+}
